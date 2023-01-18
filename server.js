2f02b19e868bde3894480519e6cf9e09fb584db4
oleend <davidpoleen@gmail.com>
2023-01-18 18:32:27 -0500

    Added Additional Things
    Continued

--- server.js
+++ server.js
@@ -3,17 +3,49 @@ const express = require('express')
 let app = express()
 
 //MongoDB Connecction
-const mongoose =require('mongoose')
+const mongoose = require('mongoose')
 
-//Connect to the Database
-mongoose.connect('mongodb://localhost/blogTesting')
-.then(function(){
-    console.log('Connection Successfull')
-})
-.catch( (error) => {
 
-    console.log(error)
+//Connect to the Database, collection(Table): Document is the model, collumns is the schema, have to use loopback numbers
+mongoose.connect('mongodb://127.0.0.1/blogTesting')
+    .then(function () {
+        console.log('Connection Successfull')
+    })
+    .catch((error) => {
+
+        console.log(error)
+    })
+
+//model and schema
+//Embedded Document Relationship
+const Author = mongoose.model("author", {
+
+
+    // WHERE WE LEFT OFF ADDING UNIQUE IDENTIFIERS
+    name: { Type: String, unique: String },
+    numberBooks: Number,
+    books: [{
+        title: String,
+        publishData: String,
+    }]
 })
+
+//Different ID for each one from within each entry
+Author.create({
+    name: 'J.K Rolling',
+    numberBooks: "5",
+    books: [{ title: "HP1", publishData: "2000" }, { title: "HP2", publishData: "2001" }, { title: "HP3", publishData: "2003" }, { title: "HP4", publishData: "2007" }, { title: "HP5", publishData: "2009" }]
+})
+
+    .then((result) => {
+        console.log(result);
+
+    })
+
+    .catch((eror) => {
+        console.log(error);
+    })
+
 
 //Server Path -- default
 app.get('/', (req, res) => {
@@ -23,7 +55,7 @@ app.get('/', (req, res) => {
         res.send('Server Error: 400')
         console.log(error)
     }
-   
+
 })
 
 
